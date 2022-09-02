fc04b39a6aad9bdb1a487ced278d0e6045169eb5
ibaslogic <ibaskunle@gmail.com>
2022-09-02 07:39:29 +0100

    image feed removed

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import { MdRssFeed } from 'react-icons/md';
 
-// const site_url = process.env.VERCEL_URL;
+const site_url = process.env.VERCEL_URL;
 
 import styles from '../styles/Footer.module.css';
 const Footer = () => {
@@ -10,8 +10,7 @@ const Footer = () => {
         <div className={styles.footer__content}>
           <p>&copy; {new Date().getFullYear().toString()} by Ibas</p>
           <a
-            // href={`${site_url}/rss.xml`}
-            href="#"
+            href={`${site_url}/rss.xml`}
             rel="noreferrer"
             target="_blank"
           >
